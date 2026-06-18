2c7e43f9221e2e4563999017dc2004a7544f3aa3
bt3762334-commits <bt3762334@gmail.com>
2026-06-18 23:30:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,15 @@ import Certificates from "./components/Certificates";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+import Background from "./components/Background";
+import Cursor from "./components/Cursor";
+
 export default function App() {
   return (
     <>
+      <Background />
+      <Cursor />
+
       <Navbar />
       <Hero />
       <About />
